14b2fcb8efb2427bd1eb707573bc950e75b37330
snow.jhung <snowwayne1231@gmail.com>
2021-05-19 15:05:14 +0800

    v1.5.17 add thread queue in tcpsocket

--- service/management/commands/testsocket.py
+++ service/management/commands/testsocket.py
@@ -1,12 +1,32 @@
 from django.core.management.base import BaseCommand
-import os, sys, getopt, socket, time
 from configparser import RawConfigParser
 
 from tcpsocket.chat_package import pack, unpack
 from dataparser.apps import ExcelParser
 
+import os, sys, socket, time, threading
+
+
+threadLimiter = threading.BoundedSemaphore(8)
+class AutoReleaseThread(threading.Thread):
+
+    def run(self):
+        threadLimiter.acquire()
+        try:
+            self.Executemycode()
+        finally:
+            threadLimiter.release()
+
+    def Executemycode(self):
+        print(" Hello World!")
+        # <your code here>
+
+
 class Command(BaseCommand):
     help = 'train models'
+    client = None
+    bufsize = 1024
+    spend_recv_second = 0
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -18,12 +38,50 @@ class Command(BaseCommand):
         parser.add_argument(
             '-host', dest='host', required=False, help='host',
         )
+        parser.add_argument(
+            '-m', dest='multiple', required=False, help='use multiple thread to test.',
+        )
+
+    def handle_recv_data(self, packed, status):
+        _start_time = time.time()
+        self.client.send(packed)
+        try:
+            recv_data = self.client.recv(self.bufsize)
+            _recv_time = time.time()
+            _spend_recv_second = _recv_time - _start_time
+            self.spend_recv_second += _spend_recv_second
+        except socket.timeout:
+            print('Recv Data Timeout.')
+            return 0
+        except Exception:
+            recv_data = None
+
+        if not recv_data:
+            print('not receive packed: {} '.format(packed))
+            return 0
+        _trying_unpacked = unpack(recv_data)
+        _is_deleted = _trying_unpacked.code > 0
+        _is_right = False
+        if _is_deleted:
+            if status > 0:
+                _is_right = True
+        else:
+            if status == 0:
+                _is_right = True
+        
+        if _is_right:
+            return 1
+        else:
+            print('Wrong Predict :: txt = [{}]   ans = [{}]'.format(_trying_unpacked.code, status))
+            return 0
+
 
     def handle(self, *args, **options):
+        _handle_start_time = time.time()
         file_path = options.get('input_excel_path', None)
         port = options.get('port')
         host = options.get('host')
-        bufsize = 1024
+        is_multiple = options.get('multiple', False)
 
         if port is None:
             port = 8025
@@ -53,10 +111,16 @@ class Command(BaseCommand):
             statuses = [0,0,0,0,0, 1, 4]
 
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        client.settimeout(3)
+        
+        # client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
         addr = (host, port)
 
         client.connect(addr)
+        
+        self.client = client
 
         cmd_ints = [0x000001, 0x040001, 0x040002, 0x040003, 0x040004]
 
@@ -68,7 +132,7 @@ class Command(BaseCommand):
         length_messages = len(messages)
 
         length_right = 0
-        
+        _threads = []
         while keep_doing and msgid < length_messages:
 
             try:
@@ -85,40 +149,28 @@ class Command(BaseCommand):
                     print('msgtxt wrong: ', msgtxt)
                     continue
                 
-                client.send(packed)
-
-                recv_data = client.recv(bufsize)
-                if not recv_data:
-                    print('not receive msgid: {}   msgtxt: {}'.format(msgid, msgtxt))
-                    break
-
-                # print('===== receive data =====')
-                # print (recv_data.decode('utf-8'))
-                trying_unpacked = unpack(recv_data)
-                _is_deleted = trying_unpacked.code > 0
-                _is_right = False
-                if _is_deleted:
-                    if status > 0:
-                        _is_right = True
-                else:
-                    if status == 0:
-                        _is_right = True
                 
-                if _is_right:
-                    length_right += 1
-                else:
-                    print('Wrong Predict :: txt = [{}]   ans = [{}]'.format(msgtxt, status))
 
+                if is_multiple:
+                    _new_thread = threading.Thread(target= self.handle_recv_data, args = (packed, status))
+                    _threads.append(_new_thread)
+                    _new_thread.start()
+                else:
+                    length_right += self.handle_recv_data(packed, status)
+                    
+                time.sleep(0.001)
                 msgid += 1
-                time.sleep(0.01)
 
             except KeyboardInterrupt:
 
                 keep_doing = False
 
-
+        
+        for t in _threads:
+            t.join()
         print('disconnect from tcp server.')
+        print('Length Of Message: ', length_messages, 'Count Of Threading: ', threading.active_count())
+        print('Spend Receive Second: ', self.spend_recv_second, 'Execute Time: ', time.time() - _handle_start_time)
         print('right ratio : {:2.2%}'.format(length_right / length_messages))
         client.close()
 
-        
--- tcpsocket/tcp.py
+++ tcpsocket/tcp.py
@@ -2,8 +2,9 @@
 from configparser import RawConfigParser
 from socketserver import StreamRequestHandler as Tcp
 from tcpsocket.chat_package import pack, unpack
+# from queue import Queue
 import logging
-import os, datetime, json
+import os, datetime, json, threading
 
 SOCKET_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(SOCKET_DIR)
@@ -22,6 +23,30 @@ sig = config_keys.get('SERVER', 'PWD')
 logging_level = logging.DEBUG if bool('True' in config_setting.get('MAIN', 'DEBUG')) else logging.INFO
 logging.basicConfig(format='[%(levelname)s]%(asctime)s %(message)s', datefmt='(%m/%d) %I:%M:%S %p :: ', level=logging_level)
 
+threadLock = threading.Lock()
+
+class LockThread(threading.Thread):
+    def __init__(self, target, args):
+        threading.Thread.__init__(self)
+        self.target = target
+        self.args = args
+
+    def run(self):
+        try:
+            if threadLock.acquire(timeout=3):
+                self.target(*self.args)
+            
+        except Exception as exp:
+
+            logging.error('LockThread Something Wrong.')
+            print(exp)
+
+        finally:
+
+            threadLock.release()
+            del self.target, self.args
+            
+
 
 class socketTcp(Tcp):
 
@@ -30,6 +55,7 @@ class socketTcp(Tcp):
     on_client_close = None
     service_instance = None
     nickname_filter_instance = None
+    # thread_queue = Queue(4)
     
 
     def __init__(self, callback, service_instance, on_client_open = None, on_client_close = None, nickname_filter_instance = None, *args, **keys):
@@ -40,7 +66,111 @@ class socketTcp(Tcp):
         self.nickname_filter_instance = nickname_filter_instance
         
         super().__init__(*args, **keys)
+
+
+    def handle_recive_threading(self, recived):
+        unpacked_data = unpack(recived)
+
+        status_code = -1
+        prediction = None
+
+        if unpacked_data.cmd == 0x000001:
+            logging.debug('Recived Package is [ Check Hearting ]')
+            packed_res = pack(0x000001) # hearting
+
+        elif unpacked_data.cmd == 0x040001:
+            logging.debug('Recived Package is [ Login ]')
+            
+            is_matched = serverid == unpacked_data.serverid and sig == unpacked_data.sig
+
+            if is_matched:
+                logging.debug('Login Successful serverid: {}'.format(serverid))
+                server_code = 0
+            else:
+                logging.debug('Login Failed. unpacked.sig: {}'.format(unpacked_data.sig))
+                server_code = 1
+
+            packed_res = pack(0x040002, code=server_code)
+
+        elif unpacked_data.cmd == 0x040002:
+            logging.debug('Recived Package is [ Login Response ]')
+            packed_res = pack(0x000001)
+
+        elif unpacked_data.cmd == 0x040003:
+            logging.debug('Recived Package is [ Chat ] msg: {}'.format(unpacked_data.msg))
+            # logging.debug('msgid: {}'.format(unpacked_data.msgid))
+            # logging.debug('msgtxt: {}'.format(unpacked_data.msg))
+            # logging.debug('msgbuffer: {}'.format(unpacked_data.msgbuffer))
+            # logging.debug('msgsize: {}'.format(unpacked_data.msgsize))
+
+            status_code = 0
+            _msg = unpacked_data.msg
+            if isinstance(unpacked_data.msgid, int) and _msg:
+                ai_results = self.service_instance.think(message=_msg)
+                prediction = ai_results.get('prediction', None)
+            
+            if prediction:
+                if prediction > 0:
+                    status_code = 5
+                    logging.info('Message be blocked = id: {} txt: {}'.format(unpacked_data.msgid, unpacked_data.msg))
+
+            packed_res = pack(0x040004, msgid=unpacked_data.msgid, code=status_code)
+
+        elif unpacked_data.cmd == 0x041003:
+            logging.debug('Recived Package is [ Chat Json ] msg: {} | room: {}'.format(unpacked_data.msg, unpacked_data.roomid))
+            # logging.debug('json string: {}'.format(unpacked_data.jsonstr))
+
+            status_code = 0
+            _msg = unpacked_data.msg
+
+            if isinstance(unpacked_data.msgid, int) and _msg:
+
+                ai_results = self.service_instance.think(message=_msg, room=unpacked_data.roomid)
+                prediction = ai_results.get('prediction', None)
+            
+
+            if prediction and prediction > 0:
+                status_code = 5
+                logging.info('Message be blocked = id: {} msg: {}'.format(unpacked_data.msgid, _msg))
+            
+
+            packed_res = pack(0x040004, msgid=unpacked_data.msgid, code=status_code)
+
+        elif unpacked_data.cmd == 0x040004:
+            logging.debug('Recived Package is [ Chat Response ]')
+            logging.debug('msgid: {}'.format(unpacked_data.msgid))
+            logging.debug('code: {}'.format(unpacked_data.code))
+            packed_res = pack(0x000001)
+
+        elif unpacked_data.cmd == 0x040007:
+            logging.debug('Recived Package is [ Nickname Change Request ] id: {} | name: {} | size: {}'.format(unpacked_data.reqid, unpacked_data.nickname, unpacked_data.size))
+
+            if self.nickname_filter_instance:
+
+                ai_results = self.nickname_filter_instance.think(nickname=unpacked_data.nickname)
+                code = ai_results.get('code', 0)
+
+                if code and code > 0:
+                    logging.info('Nickname Change Request be blocked = reqid: {} | code: {}'.format(unpacked_data.reqid, code))
+
+            else:
+
+                code = 0
+
+            packed_res = pack(0x040008, reqid=unpacked_data.reqid, code=code)
+            prediction = code
+
+        else:
+            
+            logging.error('Recived Package Unknow.')
+            packed_res = pack(0x000001)
+        
+        
+        self.request.sendall(packed_res)
+        if self.callback and prediction is not None:
+            self.callback(unpacked_data, int(prediction), status_code)
     
+
 
     def handle(self):
         logging.info('**TCPSocket Version[{}] clinet has connected, address: {}'.format(version, self.client_address))
@@ -53,112 +183,15 @@ class socketTcp(Tcp):
 
             # now = datetime.datetime.now()
             
-            unpacked_data = unpack(recived)
-
-            # packed_res = pack(0x000001)
-
-            status_code = -1
-            prediction = None
-
-            if unpacked_data.cmd == 0x000001:
-                logging.debug('Recived Package is [ Check Hearting ]')
-                packed_res = pack(0x000001) # hearting
-
-            elif unpacked_data.cmd == 0x040001:
-                logging.debug('Recived Package is [ Login ]')
-                
-                is_matched = serverid == unpacked_data.serverid and sig == unpacked_data.sig
-
-                if is_matched:
-                    logging.debug('Login Successful serverid: {}'.format(serverid))
-                    server_code = 0
-                else:
-                    logging.debug('Login Failed. unpacked.sig: {}'.format(unpacked_data.sig))
-                    server_code = 1
-
-                packed_res = pack(0x040002, code=server_code)
-
-            elif unpacked_data.cmd == 0x040002:
-                logging.debug('Recived Package is [ Login Response ]')
-                packed_res = pack(0x000001)
-
-            elif unpacked_data.cmd == 0x040003:
-                logging.debug('Recived Package is [ Chat ] msg: {}'.format(unpacked_data.msg))
-                # logging.debug('msgid: {}'.format(unpacked_data.msgid))
-                # logging.debug('msgtxt: {}'.format(unpacked_data.msg))
-                # logging.debug('msgbuffer: {}'.format(unpacked_data.msgbuffer))
-                # logging.debug('msgsize: {}'.format(unpacked_data.msgsize))
-
-                status_code = 0
-                _msg = unpacked_data.msg
-                if isinstance(unpacked_data.msgid, int) and _msg:
-                    ai_results = self.service_instance.think(message=_msg)
-                    prediction = ai_results.get('prediction', None)
-                
-                if prediction:
-                    if prediction > 0:
-                        status_code = 5
-                        logging.info('Message be blocked = id: {} txt: {}'.format(unpacked_data.msgid, unpacked_data.msg))
-
-                packed_res = pack(0x040004, msgid=unpacked_data.msgid, code=status_code)
-
-            elif unpacked_data.cmd == 0x041003:
-                logging.debug('Recived Package is [ Chat Json ] msg: {} | room: {}'.format(unpacked_data.msg, unpacked_data.roomid))
-                # logging.debug('json string: {}'.format(unpacked_data.jsonstr))
-
-                status_code = 0
-                _msg = unpacked_data.msg
-
-                if isinstance(unpacked_data.msgid, int) and _msg:
-
-                    ai_results = self.service_instance.think(message=_msg, room=unpacked_data.roomid)
-                    prediction = ai_results.get('prediction', None)
-                
-
-                if prediction and prediction > 0:
-                    status_code = 5
-                    logging.info('Message be blocked = id: {} msg: {}'.format(unpacked_data.msgid, _msg))
-                
-
-                packed_res = pack(0x040004, msgid=unpacked_data.msgid, code=status_code)
-
-            elif unpacked_data.cmd == 0x040004:
-                logging.debug('Recived Package is [ Chat Response ]')
-                logging.debug('msgid: {}'.format(unpacked_data.msgid))
-                logging.debug('code: {}'.format(unpacked_data.code))
-                packed_res = pack(0x000001)
-
-            elif unpacked_data.cmd == 0x040007:
-                logging.debug('Recived Package is [ Nickname Change Request ] id: {} | name: {} | size: {}'.format(unpacked_data.reqid, unpacked_data.nickname, unpacked_data.size))
-
-                if self.nickname_filter_instance:
-
-                    ai_results = self.nickname_filter_instance.think(nickname=unpacked_data.nickname)
-                    code = ai_results.get('code', 0)
-
-                    if code and code > 0:
-                        logging.info('Nickname Change Request be blocked = reqid: {} | code: {}'.format(unpacked_data.reqid, code))
-
-                else:
-
-                    code = 0
-
-                packed_res = pack(0x040008, reqid=unpacked_data.reqid, code=code)
-                prediction = code
-
-            else:
-                
-                logging.error('Recived Package Unknow.')
-                packed_res = pack(0x000001)
+            _thread = LockThread(target = self.handle_recive_threading, args = (recived,))
+            _thread.start()
             
-            
-            self.request.sendall(packed_res)
-            if self.callback and prediction is not None:
-                self.callback(unpacked_data, int(prediction), status_code)
+
             # self.request.sendall(recived)
         
         logging.info('**TCPSocket clinet disconnected, address: {}'.format(self.client_address))
         self.on_client_close()
+    
 
     def handle_error(self):
         logging.error('TCPSocket Handle Error!!')
